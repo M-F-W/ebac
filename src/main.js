7a4d72165a92e72078ae14682c564c210aad7c42
Matheus de Freitas Weber <matheusfweber@hotmail.com>
2024-03-15 22:25:55 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicaDoisNum(a, b) {
+    return a * b;
+}
+const dizOi = (nome) => `Olá ${nome}`;
+console.log(dizOi('Fernanda'));
+console.log(multiplicaDoisNum(2, 3));
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+function multiplicaDoisNum(a: number, b: number): number{
+    return a * b
+}
+
+const dizOi = (nome:string) => `Olá ${nome}`
+
+console.log(dizOi('Fernanda'))
+console.log(multiplicaDoisNum(2,3))
